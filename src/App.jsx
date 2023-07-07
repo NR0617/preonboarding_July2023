c10d825befdd52844a519d4e33acc902a9eebb99
NR0617 <nara20210617@gmail.com>
2023-07-07 21:02:52 +0900

    feat: redux 구현 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,48 @@
 import "./App.css";
 import { store } from "./Redux/testReducer";
+import { useState } from "react";
 
 function App() {
-  // 스토어 상태 출력
-  console.log(store.getState()); // { count: 0 }
+    const [counter, setCounter] = useState(0);
+    // 스토어 상태 출력
+    console.log(store.getState()); // { count: 0 }
+    const handleButton = (e) => {
+        console.log(e.target.name);
+        if (e.target.name === "plus") {
+            store.dispatch({ type: "plus" });
+            const { count } = store.getState();
+            console.log(count);
+            setCounter(count);
+        } else {
+            store.dispatch({ type: "minus" });
+            const { count } = store.getState();
 
-  // 디스패치 테스트
-  store.dispatch({ type: "plus" });
-  console.log(store.getState()); // { count: 1 }
+            setCounter(count);
+        }
+    };
+    // 디스패치 테스트
+    // console.log(store.getState()); // { count: 1 }
 
-  store.dispatch({ type: "minus" });
-  console.log(store.getState()); // { count: 0 }
+    // store.dispatch({ type: "minus" });
+    // console.log(store.getState()); // { count: 0 }
 
-  store.dispatch({ type: "unknown" });
-  console.log(store.getState()); // { count: 0 }
+    // store.dispatch({ type: "unknown" });
+    // console.log(store.getState()); // { count: 0 }
 
-  return (
-    <div>
-      <h1>createStore는 Redux폴더에 구현되어있습니다</h1>
-      <h3>README에 상세한 설명이 나와있어요</h3>
-      <a href="https://redux.js.org/">리덕스 홈페이지로 이동</a>
-    </div>
-  );
+    return (
+        <div>
+            <h1>createStore는 Redux폴더에 구현되어있습니다</h1>
+            <h3>README에 상세한 설명이 나와있어요</h3>
+            <button name="plus" onClick={handleButton}>
+                +
+            </button>
+            <button name="minus" onClick={handleButton}>
+                -
+            </button>
+            <div>{counter}</div>
+            <a href="https://redux.js.org/">리덕스 홈페이지로 이동</a>
+        </div>
+    );
 }
 
 export default App;
--- src/Redux/testReducer.js
+++ src/Redux/testReducer.js
@@ -1,17 +1,18 @@
 import { createStore } from "./createStore";
+
 const initialState = {
-  count: 0,
+    count: 0,
 };
 
 export const testReducer = (state = initialState, action = {}) => {
-  switch (action.type) {
-    case "plus":
-      return { ...state, count: state.count++ };
-    case "minus":
-      return { ...state, count: state.count-- };
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case "plus":
+            return { ...state, count: state.count + 1 };
+        case "minus":
+            return { ...state, count: state.count - 1 };
+        default:
+            return state;
+    }
 };
 
 export const store = createStore(testReducer);
